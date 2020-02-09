dd3420a5742f4c06ac344dd2e05b63cbf3dbfdd4
Jimmy Lin <jimmylin@Lillians-MacBook-Pro-2018.local>
2020-02-09 14:46:33 -0800

    add StacksOfStacks

--- StacknQueues.js
+++ StacknQueues.js
@@ -60,3 +60,43 @@ class Queue {
     return this.first === null;
   }
 }
+
+//Cracking the Coding Interview pg99 3.3
+class SetOfStacks {
+  constructor(limit) {
+    this.stacks = new Stack;
+    this.limit = limit;
+    this.curSize = 0;
+    this.curStack = this.stacks;
+  }
+  push(val) {
+    if(this.curSize === 0) {
+      this.curStack.top = new Stack;
+      this.curStack.top.push(val);
+      this.curSize++;
+    } else if(this.curSize === this.limit) {
+        let newStack = new Stack;
+        this.stacks.push(newStack);
+        this.curStack = newStack;
+        this.curStack.top = new Stack;
+        this.curStack.top.push(val);
+        this.curSize = 1;
+    } else {
+      this.curStack.top.push(val);
+      this.curSize++;
+    }
+  }
+  pop() {
+    if(this.curSize === 1) {
+      let popVal = this.curStack.top.pop();
+      let lastPop = this.stacks.pop();
+      this.curStack = lastPop.top.next;
+      this.curSize = this.limit;
+      return popVal;
+    } else {
+      this.curSize--;
+      return this.curStack.top.pop();
+    }
+  }
+}
+
